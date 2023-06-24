8a27f6b1845e2a66332afde909a651c633f152f4
Tae-Hyun <kt9811@github.com>
2023-06-25 00:22:49 +0900

    키오스크

--- kiosk-app/src/pages/Detail.js
+++ kiosk-app/src/pages/Detail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Detail.css';
 
@@ -9,17 +9,20 @@ export default function Detail(props) {
   const {icecreams} = props;
   const {id} = useParams();
 
-  const check = document.querySelectorAll('.select input');
-    check.forEach((item,idx)=>{
-      item.addEventListener('click',()=>{
-        if(item.checked){
-          let a = parseInt(check[idx].value)
-          console.log(a)
-          return a
-        }
-      })
-      
-    })
+  const items =[
+    {id:'1', label:'싱글레귤러', sub:'중량 115g', price:3500},
+    {id:'2', label:'싱글킹', sub:'중량 145g', price:4500},
+    {id:'3', label:'더블주니어', sub:'중량 150g', price:5500},
+    {id:'4', label:'더블레귤러', sub:'중량 230g', price:6700}
+  ]
+
+  let total=0;
+  const [opprice, setPrice] = useState(items[0])
+  const onChangeOp = (e) =>{setPrice(e.target.value)}
+  const [sum, setSum]=useState(0)
+  const onChangeSum=(e)=>{total+=setSum(e.target.value)}
+ 
+  
    
   return (
     <>
@@ -42,20 +45,24 @@ export default function Detail(props) {
     <div className='option_box'>
       <div className='option1'>
         <h2>CONE & CUP</h2>
-        <div className='select'>
-          <input type='radio' id='select1' name='sele' value='3500'/>
-          <label htmlFor='select1'>
-            <img src={process.env.PUBLIC_URL + '/assets/images/single.gif'} alt='icon'/>
+        <div>
 
+    {
+    items.map((item,i) => {
+      return(
+        <div className='select' key={i}>
+          <label>
+          <input type='radio' id={item.id} name='sele' value={item.price} onChange={onChangeSum} checked={sum===total}/>
+            <img src={process.env.PUBLIC_URL + '/assets/images/single.gif'} alt='icon'/>
           <div>
-            <h3 className='option_tit'>싱글레귤러</h3>
-            <p className='option_desc'>중량 115g</p>
-            <p className='option_price'>{icecreams[id].price}원</p>
+            <h3 className='option_tit'>{item.label}</h3>
+            <p className='option_desc'>{item.sub}</p>
+            <p className='option_price'>{item.price.toLocaleString()}원</p>
           </div>
 
           </label>
 
-          <input type='radio' id='select2' name='sele' value='3800'/>
+         {/*  <input type='radio' id='select2' name='sele' value='3800'/>
           <label htmlFor='select2'>
             <img src={process.env.PUBLIC_URL + '/assets/images/singleking.gif'} alt='icon'/>
 
@@ -85,13 +92,17 @@ export default function Detail(props) {
             <p className='option_price'>6,700원</p>
             </div>
           </label>
-
+ */}
         </div>
-      </div>
-    </div>
+      
+    )})
+  }
+ </div>
 
+  </div>
+    </div>
     <div className='total'>
-      총금액 : <span></span>원
+      총금액 : <span>{sum}</span>원
     </div>
    </>
   )
