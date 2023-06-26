5c0e52c108923d0d4adf1337030cecaea81ce7f0
Tae-Hyun <kt9811@github.com>
2023-06-26 18:23:01 +0900

    키오스크

--- kiosk-app/src/pages/DetaillDrink.js
+++ kiosk-app/src/pages/DetaillDrink.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import './Detail.css';
+
+const DetailContent = styled.div`
+  position: relative;
+  text-align: center;
+  padding-top: 40px;
+  padding-bottom: 30px;
+  border-bottom: 2px solid violet;
+`
 
 export default function Detail(props) {
 
@@ -9,21 +18,21 @@ export default function Detail(props) {
 
   return (
     <>
-    <div className='detail_box'>
+    <DetailContent className='detail_box'>
       <div>
         <img src={process.env.PUBLIC_URL + '/assets/images/spoon.png'} alt='spoon'/>
       </div>
       <h1>{icecreams[id].title}</h1>
       <p>{icecreams[id].desc}</p>
       
-      <div className='imgs'>
+      <div className='img'>
         <img className='product_img' src={icecreams[id].image} alt='img'/>
       </div>
 
       <div className='cart'>
         <a href='#!'>찜하기</a>
       </div>
-    </div>
+    </DetailContent>
    </>
   )
 }
