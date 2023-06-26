a4d2874f17d2c9bd2b93c0e684cb66a6a28eb875
TaeHyun <kt9811@naver.com>
2023-06-26 12:26:51 +0900

    키오스크

--- kiosk-app/src/pages/DetailIcecream.js
+++ kiosk-app/src/pages/DetailIcecream.js
@@ -10,19 +10,32 @@ export default function DetailIcecream(props) {
   const {id} = useParams();
 
   const items =[
-    {id:'1', label:'싱글레귤러', sub:'중량 115g', price:3500},
-    {id:'2', label:'싱글킹', sub:'중량 145g', price:4500},
-    {id:'3', label:'더블주니어', sub:'중량 150g', price:5500},
-    {id:'4', label:'더블레귤러', sub:'중량 230g', price:6700}
+    {id:'1', label:'싱글레귤러', image: process.env.PUBLIC_URL + '/assets/images/single.gif', sub:'중량 115g', price:3500},
+    {id:'2', label:'싱글킹', image: process.env.PUBLIC_URL + '/assets/images/singleking.gif', sub:'중량 145g', price:4500},
+    {id:'3', label:'더블주니어', image: process.env.PUBLIC_URL + '/assets/images/doubleju.gif', sub:'중량 150g', price:5500},
+    {id:'4', label:'더블레귤러', image: process.env.PUBLIC_URL + '/assets/images/doublere.gif', sub:'중량 230g', price:6700}
   ]
 
-  let total=0;
-  const [opprice, setPrice] = useState(items[0])
-  const onChangeOp = (e) =>{setPrice(e.target.value)}
-  const [sum, setSum]=useState(0)
-  const onChangeSum=(e)=>{total+=setSum(e.target.value)}
+  const items1 =[
+    {id:'1', label:'파인트', image: process.env.PUBLIC_URL + '/assets/images/pint.gif', sub:'중량 320g', price:8900},
+    {id:'2', label:'쿼터', image: process.env.PUBLIC_URL + '/assets/images/quarter.gif', sub:'중량 620g', price:17000},
+    {id:'3', label:'패밀리', image: process.env.PUBLIC_URL + '/assets/images/family.gif', sub:'중량 960g', price:24000},
+    {id:'4', label:'하프갤런', image: process.env.PUBLIC_URL + '/assets/images/half.gif', sub:'중량 1200g', price:29000}
+  ]
 
+  const [opprice, setPrice] = useState(3500)
+  const [sum, setSum] = useState(5500)
 
+  //선택된 value값 받는거
+  const onChangeOp = (e) =>{setPrice(parseInt(e.target.value))}
+  const onCheckOp = (checked,value) => {
+    if(checked){
+      
+    }
+  }
+  const onChangeSum=(e)=>{setSum(parseInt((e.target.value)))}
+  
+  let total=opprice+sum;
 
   return (
     <>
@@ -44,17 +57,18 @@ export default function DetailIcecream(props) {
     </div>
 
     <div className='option_box'>
-      <div className='option1'>
+      <div className='option1 option'>
         <h2>CONE & CUP</h2>
-        <div>
+
+        <div className='select'>
 
     {
-    items.map((item,i) => {
+    items.map((item, i) => {
       return(
-        <div className='select' key={i}>
-          <label>
-          <input type='radio' id={item.id} name='sele' value={item.price} onChange={onChangeSum} checked={sum===total}/>
-            <img src={process.env.PUBLIC_URL + '/assets/images/single.gif'} alt='icon'/>
+        <>
+          <label  key={i}>
+          <input type='radio' id={item.id} name='sele' value={item.price} onChange={onChangeOp} checked={onCheckOp(item.checked,setPrice)}/>
+            <img src={item.image} alt='icon'/>
           <div>
             <h3 className='option_tit'>{item.label}</h3>
             <p className='option_desc'>{item.sub}</p>
@@ -94,16 +108,37 @@ export default function DetailIcecream(props) {
             </div>
           </label>
  */}
-        </div>
-      
+        </>
     )})
   }
- </div>
-
+    </div>
   </div>
+
+  <div className='option2 option'>
+    <h2>HAND PACK</h2>
+
+  <div className='select2'>
+  {
+    items1.map((item,i) => {
+      return (
+          <label key={i}>
+          <input type='radio' id={item.id} name='seleop' value={item.price} onChange={onChangeSum} checked={onCheckOp(item.checked, setSum)}/>
+            <img src={item.image} alt='icon'/>
+          <div>
+            <h3 className='option_tit'>{item.label}</h3>
+            <p className='option_desc'>{item.sub}</p>
+            <p className='option_price'>{item.price.toLocaleString()}원</p>
+          </div>
+
+          </label>
+      )
+    })
+  }
+        </div>
+        </div>
     </div>
     <div className='total'>
-      총금액 : <span>{sum}</span>원
+      총금액 : <span>{total.toLocaleString()}</span>원
     </div>
    </>
   )
