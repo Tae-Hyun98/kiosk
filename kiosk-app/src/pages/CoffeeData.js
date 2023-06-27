5a2616f0de2bdeee394ae8526d6148243bfde432
TaeHyun <kt9811@naver.com>
2023-06-27 14:27:44 +0900

    키오스크

---
+++ kiosk-app/src/pages/CoffeeData.js
@@ -0,0 +1,30 @@
+const coffees = [
+  {
+    id: 'coffee01',
+    image: process.env.PUBLIC_URL + '/assets/images/coffee/coffee01.png',
+    title: '917 아메리카노',
+    tag: '#대용량커피 #아이스아메리카노',
+    desc: '917ml사이즈로 즐기는 대용량 브리즈 아이스 아메리카노',
+    price: 3800
+  },
+
+  {
+    id: 'coffee02',
+    image: process.env.PUBLIC_URL + '/assets/images/coffee/coffee02.png',
+    title: '디카페인 917 아메리카노(ICED)',
+    tag: '#대용량커피 #디카페인',
+    desc: '917ml사이즈로 즐기는 대용량 브리즈 디카페인 아이스 아메리카노',
+    price: 4000
+  },
+
+  {
+    id: 'coffee03',
+    image: process.env.PUBLIC_URL + '/assets/images/coffee/coffee03.png',
+    title: '피스타치오 초코 아몬드 라떼',
+    tag: '#대용량커피 #디카페인',
+    desc: '917ml사이즈로 즐기는 대용량 브리즈 디카페인 아이스 아메리카노',
+    price: 4000
+  },
+]
+
+export default coffees
