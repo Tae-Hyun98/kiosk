c32c9e1dca8c81be0ee6122641941b425793f852
TaeHyun <kt9811@naver.com>
2023-06-27 14:07:56 +0900

    키오스크

--- kiosk-app/src/components/Header.js
+++ kiosk-app/src/components/Header.js
@@ -2,25 +2,36 @@ import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux';
-import {motion} from 'framer-motion';
 
 const Inner = styled.div`
   width: 1200px;
   margin: 0 auto;
 `
+const BackBtn = styled.button`
+  position: absolute;
+  left: 0;
+  top: 15px;
+  font-size: 0;
+  width: 60px;
+  height: 60px;
+  cursor: pointer;
+  background-color: #fff;
+  background-size: 30px;
+  background-position: 50% 50%;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+`
+
 export default function Header() {
   let navigate = useNavigate();
   const state = useSelector((state)=>state)
 
   return (
     <>
-      <motion.header className='header'
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:0.8}}
-      >
+      <header className='header'>
         <Inner>
-          <button className='back' onClick={() => {navigate(-1)}}>뒤로가기</button>
+          <BackBtn className='back' onClick={() => {navigate(-1)}}>뒤로가기</BackBtn>
+          
           <Link to='/' className='logo'>
             <img src={process.env.PUBLIC_URL + '/assets/images/logo.png'} alt='img'/>
           </Link>
@@ -31,7 +42,7 @@ export default function Header() {
 
           </Link>
         </Inner>
-      </motion.header>
+      </header>
     </>
   )
 }
