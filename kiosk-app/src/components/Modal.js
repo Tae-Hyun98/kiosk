2db399d8c2fbdd796d4696196f40988d4a80c0cd
TaeHyun <kt9811@naver.com>
2023-06-30 09:16:06 +0900

    키오스크

--- kiosk-app/src/components/Modal.js
+++ kiosk-app/src/components/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ModalBox = styled.div`
+const ModalContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -10,19 +10,40 @@ const ModalBox = styled.div`
   background-color: rgba(0,0,0,0.8);
   z-index: 1;
 `
-
+const ModalBox = styled.div`
+  position: relative;
+  width: 50%;
+  height: 300px;
+  transform: translate(50%,50%);
+  background-color: #fff;
+  border: 2px solid #ccc;
+`
+const TextBox = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+`
 const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  cursor: pointer;
+  border: 1px solid #ccc;
 
 `
 
 export default function Modal({isOpen,closeModal,addCarts}) {
 
   return (
-    <ModalBox style={{display:isOpen ? 'block':'none'}}>
-      <h2>장바구니에 담으시겠습니까?</h2>
-
-      <button onClick={addCarts}>확인</button>
-      <button onClick={closeModal}>취소</button>
-    </ModalBox>
+    <ModalContainer style={{display:isOpen ? 'block':'none'}}>
+      <ModalBox>
+        <TextBox>
+          <h2>장바구니에 담으시겠습니까?</h2>
+          <div className='btn_box'>
+            <Button onClick={addCarts}>확인</Button>
+            <Button onClick={closeModal}>취소</Button>
+          </div>
+        </TextBox>
+      </ModalBox>
+    </ModalContainer>
   )
 }
